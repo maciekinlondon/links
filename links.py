4916598c4bcf9455bc773a18afe354892215c68e
maciekinlondon <74024988+maciekinlondon@users.noreply.github.com>
2022-03-27 22:04:02 +0100

    Update links.py

--- links.py
+++ links.py
@@ -1,10 +1,10 @@
 
-link = {1: {'page': 'https://www.jw.org/en/library/magazines/awake-no1-2020-mar-apr/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/202003/wpub/g_E_202003_lg.jpg'},
-        2: {'page': 'https://www.jw.org/en/library/magazines/awake-no2-2020-jul-aug/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/202007/wpub/g_E_202007_lg.jpg'},
-        3: {'page': 'https://www.jw.org/en/library/magazines/watchtower-no2-2020-may-jun/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/wp/E/202005/wpub/wp_E_202005_lg.jpg'},
-        4: {'page': 'https://www.jw.org/en/library/magazines/watchtower-no1-2021-jan-feb/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/wp/E/202101/wpub/wp_E_202101_lg.jpg'},
-        5: {'page': 'https://www.jw.org/en/library/magazines/awake-no3-2020-nov-dec/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/202011/wpub/g_E_202011_lg.jpg'},
-        6: {'page': 'https://www.jw.org/ase/library/magazines/awake-no3-2019-nov-dec/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/201911/wpub/g_E_201911_md.jpg'},
+link = {1: {'page': 'https://www.jw.org/en/library/magazines/awake-no3-2021-nov-dec/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/202111/wpub/g_E_202111_lg.jpg},
+        2: {'page': 'https://www.jw.org/en/library/magazines/awake-no2-2021-jul-aug/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/202107/wpub/g_E_202107_lg.jpg'},
+        3: {'page': 'https://www.jw.org/en/library/magazines/awake-no1-2021-mar-apr/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/g/E/202103/wpub/g_E_202103_lg.jpg},
+        4: {'page': 'https://www.jw.org/en/library/magazines/watchtower-no1-2022-jan-feb/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/wp/E/202205/wpub/wp_E_202205_lg.jpg},
+        5: {'page': 'https://www.jw.org/en/library/magazines/watchtower-no3-2021-sep-oct/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/wp/E/202109/wpub/wp_E_202109_lg.jpg'},
+        6: {'page': 'https://www.jw.org/en/library/magazines/watchtower-no2-2021-may-jun/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/a/wp/E/202105/wpub/wp_E_202105_lg.jpg'},
         7: {'page': 'https://www.jw.org/en/jehovahs-witnesses/request-a-visit/video-why-study-the-bible/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/m/502013341/univ/art/502013341_univ_lss_lg.jpg'},
         8: {'page': 'https://www.jw.org/en/jehovahs-witnesses/memorial/remember-jesus-death/', 'thumbnail': 'https://assetsnffrgf-a.akamaihd.net/assets/m/502019250/univ/art/502019250_univ_wsr_2_lg.jpg'},
         }
